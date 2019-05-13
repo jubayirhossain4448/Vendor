3b058dc79eb87a490ff91a656a8ffa8f0daff8ba
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-05-13 23:23:11 +0600

    Question_solve_13

---
+++ Examples/Question13/src/com/jubayir/Test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jubayir;
+
+/**
+ *
+ * @author MD.JUBAYIR HOSSAIN
+ */
+public class Test {
+    public static void main(String[] args) {
+        int a [] = {1, 2, 3, 4, 5};
+        for(XXX){
+            System.out.println(a[e]);
+        }
+    }
+    
+}
