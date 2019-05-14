ab5b4c7beb3d0df447cc00f9c46f45a8af99201a
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-05-14 23:01:15 +0600

    Question_solve_17

--- Examples/Question_16/src/com/jubayir/Test.java
+++ Examples/Question_16/src/com/jubayir/Test.java
@@ -1,9 +1,11 @@
 package com.jubayir;
 
 public class Test {
+
     public static void main(String[] args) {
-        CheckingAccount acct = new CheckingAccount(int)(Math.random()*1000);  // line n1
+        CheckingAccount acct = new CheckingAccount(int)(Math.random() * 1000
+        );  // line n1
         System.out.println(acct.getAmount());
     }
-    
+
 }
---
+++ Examples/Question_17/src/com/jubayir/Color.java
@@ -0,0 +1,10 @@
+package com.jubayir;
+
+public class Color {
+    String shirts[][] = new String[2][2];
+    shirts[0][0] = "red";
+    shirts[0][1] = "blue";
+    shirts[1][0] = "small";
+    shirts[1][1] = "medium";
+    
+}
