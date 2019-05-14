7f3a67ec5eb7fc34ec099af46160d3d6755a55c4
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-05-14 23:29:06 +0600

    Question_solve_20

---
+++ Examples/Question_20/src/com/jubayir/Test.java
@@ -0,0 +1,16 @@
+package com.jubayir;
+
+public class Test {
+    public static void main(String[] args) {
+        boolean opt = true;
+        switch (opt){
+            case true;
+                System.out.println("True");
+                break;
+                default;
+                    System.out.println("***");
+        }
+        System.out.println("Done");
+    }
+    
+}
