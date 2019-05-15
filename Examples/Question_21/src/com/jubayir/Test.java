f2645743898932fedb6a3e685399192732147407
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-05-15 22:26:21 +0600

    Question_solve_21

---
+++ Examples/Question_21/src/com/jubayir/Test.java
@@ -0,0 +1,12 @@
+package com.jubayir;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int num = 5;
+        do {
+            System.out.println(num-- + " ");
+        } while (num == 0);
+    }
+
+}
