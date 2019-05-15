bd463b01035a2619e5003e2172f140924ad798f9
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-05-15 22:50:23 +0600

    Question_solve_23

--- Examples/Questin_22/src/com/jubayir/Test.java
+++ Examples/Questin_22/src/com/jubayir/Test.java
@@ -1,13 +1,14 @@
 package com.jubayir;
 
 public class Test {
+
     public static void main(String[] args) {
         int x = 100;
         int a = x++;
         int b = ++x;
         int c = x++;
-        int d = (a <b) ? (a < c) ? a: (b <c)? b:c;
+        int d = (a < b) ? (a < c) ? a : (b < c) ? b : c;
         System.out.println(d);
     }
-    
+
 }
