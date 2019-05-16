dfc8543b0a6c9ada262583049248643922fd06fc
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-05-16 23:21:21 +0600

    Question_solve_28

---
+++ Examples/Question_28/src/com/jubayir/Test.java
@@ -0,0 +1,13 @@
+package com.jubayir;
+
+public class Test {
+    public static void main(String[] args) {
+        /* insert code here */
+        int[] array;
+        array = int[2];
+        array[0] = 10;
+        array[1] = 20;
+        System.out.println(array[0] + ":" + array[1]);
+    }
+                
+}
