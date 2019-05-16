d9c040e01a37fa85b85badc2edce4460f7ff6f2e
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-05-16 23:48:43 +0600

    Question_solve_29

---
+++ Examples/Question_29/src/com/jubayir/Test.java
@@ -0,0 +1,19 @@
+package com.jubayir;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String[] arr = {"A", "B", "C", "D"};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+            if (arr[i].equals("C")) {
+
+                continue;
+            }
+            System.out.println("Work done");
+            break;
+
+        }
+    }
+
+}
