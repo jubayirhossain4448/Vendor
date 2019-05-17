3cf7502d62bb9b277e5a53373aa0b337ede6a4a2
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-05-17 22:32:10 +0600

    Question_solve_41

--- Examples/Question_41/src/com/jubayir/CCMask.java
+++ Examples/Question_41/src/com/jubayir/CCMask.java
@@ -4,10 +4,10 @@ public class CCMask {
 
     public static String maskCC(String creditCard) {
         String x = "XXXX-XXXX-XXXX";
-// line n1
-    }
-
-    public static void main(String[] args) {
-        System.out.println("1234-5678-9101-1121");
+        // line n1
+//        return x + creditCard.substring(15, 19);
+        StringBuilder sb = new StringBuilder(x);
+        sb.append(creditCard, 15, 19);
+        return sb.toString();
     }
 }
---
+++ Examples/Question_41/src/com/jubayir/Test.java
@@ -0,0 +1,9 @@
+package com.jubayir;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("1234-5678-9101-1121");
+    }
+
+}
