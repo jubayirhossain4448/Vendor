45767dacb480fa368b70295e53a3ab7920a93c44
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-05-17 22:15:48 +0600

    Question_solve_40

---
+++ Examples/Question_40/src/com/jubayir/Test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jubayir;
+
+/**
+ *
+ * @author MD.JUBAYIR HOSSAIN
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String[] planets = {" Mercury", "Venus", "Earth", "Mars"};
+        System.out.println(planets.length);
+        System.out.println(planets[1].length());
+    }
+
+}
