b82d22255f195f92b3c342bbf3b788c87c361a31
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-05-17 22:24:59 +0600

    Question_solve_41

---
+++ Examples/Question_41/src/com/jubayir/CCMask.java
@@ -0,0 +1,13 @@
+package com.jubayir;
+
+public class CCMask {
+
+    public static String maskCC(String creditCard) {
+        String x = "XXXX-XXXX-XXXX";
+// line n1
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1234-5678-9101-1121");
+    }
+}
