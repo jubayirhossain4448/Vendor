74d73978fa667a23e5fa14fa82b6cc98040ec54e
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-05-18 00:14:32 +0600

    Question_solve_47

---
+++ Examples/Question_46/src/com/jubayir/Person.java
@@ -0,0 +1,21 @@
+package com.jubayir;
+
+public class Person {
+
+    String name;
+    int age;
+
+    public Person(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
