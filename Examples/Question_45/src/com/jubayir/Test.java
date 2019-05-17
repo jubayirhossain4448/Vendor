f6c9424ae63e6ef6d2f42e8e5a48ee4f02df1ca8
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-05-18 00:02:31 +0600

    Question_solve_45

---
+++ Examples/Question_45/src/com/jubayir/Test.java
@@ -0,0 +1,9 @@
+package com.jubayir;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("5 + 2 = " + 3 + 4);
+        System.out.println("5 + 2 = " + (3 + 4));
+    }
+    
+}
