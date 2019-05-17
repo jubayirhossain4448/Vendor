e5bd540e751cd5acc6af4256c2adb053c12592e0
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-05-17 18:12:18 +0600

    Question_solve_35

---
+++ Examples/Question_35/src/com/jubayir/Test.java
@@ -0,0 +1,13 @@
+package com.jubayir;
+
+public class Test {
+
+    public static void main(String[] args) {
+        StringBuilder sb1 = new StringBuilder("Duke");
+        String str1 = sb1.toString();
+//        String str2 = str1;
+        // insert code here
+        System.out.println(str1 == str2);
+    }
+
+}
