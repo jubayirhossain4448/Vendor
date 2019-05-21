7291b7f635a2827debd07770eb2c9ba82a39cb37
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-05-22 00:19:36 +0600

    Question_solve_82

---
+++ Examples/Question_82/src/com/jubayir/MyClass.java
@@ -0,0 +1,11 @@
+package com.jubayir;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+        String s = "Java Duke";
+        int len = s.trim().length();
+        System.out.println(len);
+    }
+
+}
