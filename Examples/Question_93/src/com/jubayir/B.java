0172a996c3c25f375f72b46a9755c5ee98ffd521
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-09 23:11:25 +0600

    Question_93

---
+++ Examples/Question_93/src/com/jubayir/B.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jubayir;
+
+/**
+ *
+ * @author MD.JUBAYIR HOSSAIN
+ */
+public class B extends A{
+    public void test(){
+        System.out.println("B");
+    }
+    
+}
