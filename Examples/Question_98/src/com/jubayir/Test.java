782b307b1b2b11536e3a7dbd671d2f1a5f47bf58
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-16 18:29:39 +0600

    Question_98

---
+++ Examples/Question_98/src/com/jubayir/Test.java
@@ -0,0 +1,9 @@
+package com.jubayir;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Result A " + (0) + 1);
+        System.out.println("Result B " + (1) + (2) );
+    }
+    
+}
