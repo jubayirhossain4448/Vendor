76cc98ed40b80d40f97faa55d5a57fd4511896b7
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-16 18:24:04 +0600

    Question_96

---
+++ Examples/Question_96/src/com/jubayir/Test.java
@@ -0,0 +1,17 @@
+package com.jubayir;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int numbers[] = {12, 13, 42, 32, 15, 156, 23, 51, 12};
+        int max = findMax(numbers);
+    }
+
+    /* line n1 */
+    static int findMax(int [] numbers){
+        int max = 0;
+        /* code goes here */
+        return max;
+    }
+
+}
