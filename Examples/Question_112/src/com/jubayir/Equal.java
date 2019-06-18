7d74b33e7a1eefa9be292fd26e6e73efd2330638
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-18 12:56:45 +0600

    Question_112

---
+++ Examples/Question_112/src/com/jubayir/Equal.java
@@ -0,0 +1,18 @@
+package com.jubayir;
+
+public class Equal {
+
+    public static void main(String[] args) {
+        String str1 = "Java";
+        String[] str2 = {"J", "a", "v", "a"};
+        String str3 = "";
+        for (String str : str2) {
+            str3 = str3 + str;
+        }
+
+        boolean b1 = (str1 == str3);
+        boolean b2 = (str1.equals(str3));
+        System.out.println(b1 + ", " + b2);
+    }
+
+}
