47e97a273bacb0167e3be5d771215825e4d45421
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-18 12:15:06 +0600

    Question_102

---
+++ Examples/Question_101/src/com/jubayir/Test.java
@@ -0,0 +1,22 @@
+package com.jubayir;
+
+public class Test {
+
+    static int count = 0;
+    int i = 0;
+
+    public void changeCount() {
+        while (i < 5) {
+            i++;
+            count++;
+        }
+    }
+
+    public static void main(String[] args) {
+        Test check1 = new Test();
+        Test check2 = new Test();
+        check1.changeCount();
+        check2.changeCount();
+        System.out.println(check1.count + " : " + check2.count);
+    }
+}
