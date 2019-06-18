7874448fca7c051f713805b781c38de92acaaee2
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-18 12:24:05 +0600

    Question_104

---
+++ Examples/Question_103/src/com/jubayir/Test104.java
@@ -0,0 +1,11 @@
+package com.jubayir;
+
+
+public class Test104 {
+    public static void main(String[] args) {
+        float flt = 100F;
+        double y1 = 203.22;
+        float flt = y1;
+    }
+    
+}
