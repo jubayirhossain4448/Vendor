8eaca5b849af4af9fbde95ba8c96c0cc8144ce9d
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-19 00:21:12 +0600

    Question_115

---
+++ Examples/Question_115/src/com/jubayir/Test.java
@@ -0,0 +1,11 @@
+package com.jubayir;
+
+public class Test {
+
+    public static void main(String[] args) {
+        float var1 = (12_345.01 >= 123_45.00) ? 12_456 : 124_56.02f;
+        float var2 = var1 + 1024;
+        System.out.println(var2);
+    }
+
+}
