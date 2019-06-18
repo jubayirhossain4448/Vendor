7705a123173ea43f1ed922870f03fab5a9512d41
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-18 23:32:45 +0600

    Question_110

---
+++ Examples/Question_110/src/com/jubayir/Test.java
@@ -0,0 +1,17 @@
+package com.jubayir;
+
+public class Test {
+    public static void main(String[] args) {
+        int data[] = {2010, 2013, 2014, 2015, 2014};
+        int key = 2014;
+        int count = 0;
+        for(int e : data){
+        if(e! = key){
+        continue;
+        count++;
+        }
+        }
+        System.out.println(count +"Found");
+    }
+    
+}
