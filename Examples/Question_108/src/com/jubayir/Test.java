581d434586e53b2163517072770755180fac22ec
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-18 12:48:43 +0600

    Question_108

---
+++ Examples/Question_108/src/com/jubayir/Test.java
@@ -0,0 +1,12 @@
+package com.jubayir;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String myStr = "Hello World";
+        myStr.trim();
+        int i1 = myStr.indexOf(" ");
+        System.out.println(i1);
+    }
+
+}
