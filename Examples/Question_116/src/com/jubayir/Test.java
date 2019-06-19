557b82021bd7faf883cd13912049e4a9cf8c01e3
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-19 19:11:49 +0600

    Question_116

---
+++ Examples/Question_116/src/com/jubayir/Test.java
@@ -0,0 +1,21 @@
+package com.jubayir;
+
+public class Test {
+
+    public static int stVar = 100;
+    public int var = 200;
+
+    public String toString() {
+        return var + ":" + stVar;
+    }
+
+    public static void main(String[] args) {
+        Test t1 = new Test();
+        t1.var = 300;
+
+        System.out.println(t1);
+        Test t2 = new Test();
+        t2.stVar = 300;
+        System.out.println(t2);
+    }
+}
