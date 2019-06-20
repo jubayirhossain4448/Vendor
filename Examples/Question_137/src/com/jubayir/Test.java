8bfd1e83f3a7b63a8027465e4b7697993b1e3bb7
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-20 11:07:43 +0600

    Question_137

---
+++ Examples/Question_137/src/com/jubayir/Test.java
@@ -0,0 +1,16 @@
+package com.jubayir;
+
+public class Test {
+    public static void main(String[] args) {
+        Test ts = new Test();
+        System.out.println(isAvailable + " ");
+        isAvailable = ts.doStuff(){
+        System.out.println(isAvailable);
+    }
+        public static boolean doStuff(){
+        return !isAvailable;
+        }
+        static boolean isAvailable = false;
+    }
+    
+}
