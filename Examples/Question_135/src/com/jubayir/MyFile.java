dcbd8c24b27ce03d3704c0a4cee405ab775fad1d
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-20 11:01:27 +0600

    Question_136

---
+++ Examples/Question_135/src/com/jubayir/MyFile.java
@@ -0,0 +1,12 @@
+package com.jubayir;
+
+public class MyFile {
+
+    public static void main(String[] args) {
+        String arg1 = args[1];
+        String arg2 = args[2];
+        String arg3 = args[3];
+        System.out.println("Arg is " + arg3);
+    }
+
+}
---
+++ Examples/Question_136/src/com/jubayir/Test.java
@@ -0,0 +1,13 @@
+package com.jubayir;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] arr = (1, 2, 3, 4);
+        int i = 0;
+        do{
+            System.out.println(arr[i] + " ");
+            i++;
+        }while(i < arr.length - 1);
+    }
+    
+}
