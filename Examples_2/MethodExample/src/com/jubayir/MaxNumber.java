592ac292f91504673b9b74e2717fcbb0ae883a7f
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-22 00:09:22 +0600

    Method_Example

---
+++ Examples_2/MethodExample/src/com/jubayir/MaxNumber.java
@@ -0,0 +1,22 @@
+package com.jubayir;
+
+public class MaxNumber {
+
+    public static int max(int num1, int num2) {
+
+        int result;
+
+        if (num1 > num2) {
+            result = num1;
+        } else {
+            result = num2;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
